b552301bc922abd2c53b22f41eb778ba61f4d847
UnknownAeon <kpfeifle@gmu.edu>
2017-08-29 18:13:33 -0400

    new meme

--- src/commands/fun/meme.js
+++ src/commands/fun/meme.js
@@ -13,6 +13,7 @@ var memeSpeak = function(msg, args) {
   if (args[0] == 'earthquake') meme = 'get ready for another chinese earth quake abrbrbrbrbrbrrbrbrbrrbuhhhhhhhhhhhhhhhhhhhhhbrbrbrbrbrbrruhhhhhhhhhhhhhhhhhh';
   else if (args[0] == 'meow') meme = 'meow. meow. meow. meow. meow. meow. meow. meow.';
   else if (args[0] == 'sprinkler') meme = 'my sprinkler goes like thisstststststststststststststststststststststststst and comes back like ttttttttttttttttttttttttttttttttttttttte';
+  else if (args[0] == 'dad') meme = 'dad daad daaad daaaad daaad daad dad dad dad daad daaad daaad daaad dadadad dad dad... dad daad daaad daaaad daaad daad dad';
   else {
     msg.reply('sorry, I do not know that one. Ask my master to teach me?');
     return;

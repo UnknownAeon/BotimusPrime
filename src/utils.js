2cbca828fe53f2b21e44635a3f2b01aa96b2eab3
UnknownAeon <kpfeifle@gmu.edu>
2017-08-29 18:13:20 -0400

    added playAudio util

--- src/utils.js
+++ src/utils.js
@@ -3,6 +3,29 @@
  * @since 0.2
  */
 
+ /**
+  * Play a specific sound clip as passed in by happySouls
+  * @param audio string/char set audio String of audio clip\
+  * @param msg the message that summoned the bot.
+  */
+ function playAudio(audio, msg) {
+   var voiceChannel = msg.member.voiceChannel;
+   try {
+     voiceChannel.join().then(function(connection) {
+       const dispatcher = connection.playFile('./assets/' + audio);
+       dispatcher.on('end', function(end) {
+         voiceChannel.leave();
+       });
+     }).catch(function(err) {
+       console.log('err');
+     });
+   }
+   catch (err) {
+     msg.reply('You need to be in a voice channel.');
+   }
+   return;
+ }
+
 /**
  * Gets a random number between 1 and the upper limit.
  * @param num the upper limit for the random number.
@@ -13,5 +36,6 @@ var random = function(num) {
 }
 
 module.exports = {
-  random : random
+  random : random,
+  playAudio : playAudio
 }

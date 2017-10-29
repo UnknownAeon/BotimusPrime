7434473865cec262194f1ae6d19a577cc5c3e1e8
UnknownAeon <kpfeifle@gmu.edu>
2017-10-28 23:55:38 -0400

    test 3

--- src/commands/info/help.js
+++ src/commands/info/help.js
@@ -10,15 +10,15 @@
 function help(command) {
   if (command.arg == undefined) {
     command.msg.reply('!help gives details on commands, to see a listing of all commands try !commands.\n\n' +
-    'Name: Help Categories\nUsage: !help [category]\nDescription: Gives description of command categories.\n' +
+    'Name: Help Categories\nUsage: !help [category]\nDescription: Gives description of command categories. ' +
     'Valid categories: fun, game, info, moderation, utility\n\n' +
-    'Name: Help Commands\nUsage: !help [category] [command] gives description of a command. See categories for listings.');
+    'Name: Help Commands\nUsage: !help [category] [command]\nDescription: Gives description of a command. See categories for listings.');
   }
   if (command.arg == 'fun') {
     if (command.subarg == undefined) {
       command.msg.reply('Commands in the fun category are those who have no purpose or are silly. Try using this command:\n' +
-      'Name: Fun Commands Help\nUsage: !help [fun] [command]\nDescription: Gives details on the given \'fun\' command.\n' +
-      'Valid commands within the \'fun\' category:\naudio\nexposed\ninsult\nmeme');
+      'Name: Fun Commands Help\nUsage: !help [fun] [command]\nDescription: Gives details on the given \'fun\' command. ' +
+      'Valid commands within the \'fun\' category: audio, exposed, insult, meme');
     }
   }
 }

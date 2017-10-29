a62b9ea3a0546f3bfa184a311aba293f8312e8f3
UnknownAeon <kpfeifle@gmu.edu>
2017-10-28 23:01:36 -0400

    updating the help feature

--- src/commands/info/help.js
+++ src/commands/info/help.js
@@ -7,10 +7,18 @@
  * Controls the bot to list out all of the available commands.
  * @param msg The message that was used to summon the bot.
  */
-function help(msg, args) {
-  if (args.length == 0) {
-    msg.reply('I am Botimus Prime, the 2AM Gaming bot. If you need help with commands, try one of these help options:\
-    \n!help [category]\nValid categories - fun, game, info, moderation, utility');
+function help(command) {
+  if (command.arg == undefined) {
+    msg.reply('Usage: !help [category]\nGives description of command categories.\n\
+     Valid categories: fun, game, info, moderation, utility\n\
+     Usage: !help [category] [command] gives description of a command.');
+  }
+  if (command.arg == 'fun') {
+    if (command.subarg == undefined) {
+      msg.reply('Commands in the fun category are those who have no purpose or are silly. Try using this command:\n\
+      !help [fun] [command]\n Valid commands within the \'fun\' category:\
+      \naudio\nexposed\ninsult\nmeme');
+    }
   }
 }
 

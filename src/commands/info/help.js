00c17507b9a759c2f847c5f800f5d68b25983b96
UnknownAeon <kpfeifle@gmu.edu>
2017-10-28 23:17:58 -0400

    small bug fix

--- src/commands/info/help.js
+++ src/commands/info/help.js
@@ -9,13 +9,13 @@
  */
 function help(command) {
   if (command.arg == undefined) {
-    msg.reply('Usage: !help [category]\nGives description of command categories.\n\
+    command.msg.reply('Usage: !help [category]\nGives description of command categories.\n\
      Valid categories: fun, game, info, moderation, utility\n\
      Usage: !help [category] [command] gives description of a command.');
   }
   if (command.arg == 'fun') {
     if (command.subarg == undefined) {
-      msg.reply('Commands in the fun category are those who have no purpose or are silly. Try using this command:\n\
+      command.msg.reply('Commands in the fun category are those who have no purpose or are silly. Try using this command:\n\
       !help [fun] [command]\n Valid commands within the \'fun\' category:\
       \naudio\nexposed\ninsult\nmeme');
     }

5177446a3692e545bb7de46a5aff322aaffd035d
UnknownAeon <kpfeifle@gmu.edu>
2017-10-30 01:25:52 -0400

    help framework

--- src/commands/info/help.js
+++ src/commands/info/help.js
@@ -3,6 +3,9 @@
  * @since 0.2
  */
 
+var commands = require('../../router.js');
+var utils = require('../../utils.js');
+
 /**
  * Controls the bot to give the help info for a given commmand.
  * @param msg The message that was used to summon the bot.
@@ -12,11 +15,25 @@ function help(command) {
     command.msg.reply('you have not specified which command you would like help with.\n\n' +
     '**__Command Name:__**\n\thelp\n\n' +
     '**__Command Usage:__**\n\t!help [command]\n\t!help [command] [subcommmand]\n\n' +
-    '**__Command Description:__**\n\n\tdisplays information for the given command or subcommand.\n\n' +
+    '**__Command Description:__**\n\tdisplays information for the given command or subcommand.\n\n' +
     'If you would like a listing of all commands, try !commands.');
   }
   else {
+    if (!utils.isValid(command.command)) command.msg.reply('that is not a valid command. Use !commands for a listing of all commands.');
+    else {
+      var commandInfo = utils.getInfo(command.command);
+      if (commandInfo.argNum == 0) {
+        command.msg.reply('\n**__Command Name:__**\n\t' + commandInfo.name + '\n\n' +
+        '**__Command Usage:__**\n\t' + commandInfo.usage + '\n\n' +
+        '**__Command Description:__**\n\t' + commandInfo.desc + '\n\n');
+      }
+      else if (commandInfo.argNum == 1) {
 
+      }
+      else if (commandInfo.argNum == 2) {
+
+      }
+    }
   }
 }
 
--- src/utils.js
+++ src/utils.js
@@ -6,11 +6,29 @@
 const commands = require('./router.js');
 
 function getInfo(command) {
-  if (command == '!cointoss') return commands.cointoss.info;
+  if (results.command === '!cointoss') return commands.cointoss.info;
+  else if (results.command === '!exposed') return commands.exposed.info;
+  else if (results.command === '!diceroll') return commands.diceroll.info;
+  else if (results.command === '!audio') return commands.audio.info;
+  else if (results.command === '!help') return commands.help.info;
+  else if (results.command === '!hots') return commands.hots.info;
+  else if (results.command === '!insult') return commands.insult.info;
+  else if (results.command === '!memespeak') return commands.meme.info;
+  else if (results.command === '!ping') {
+    return {
+      argNum : 0,
+      name : 'ping',
+      usage : '!ping',
+      desc : 'pong!'
+    };
+  }
+  else if (results.command === '!rlstats') return commands.rlstats.info;
 }
 
 function isValid(command) {
-  if (command == '!cointoss') return true;
+  var validCommands = ['!cointoss', '!exposed', '!diceroll', '!audio', '!help', '!hots', '!insult', '!memespeak', '!ping', '!rlstats'];
+  if (validCommands.indexOf(command) > -1) return true;
+  else return false;
 }
 
 /**

78a2b73c28b0b9d5b03975c6f395e77197fb8bb9
UnknownAeon <kpfeifle@gmu.edu>
2017-10-29 23:52:02 -0400

    files for pushing dev branch to heroku for testing

--- push_heroku.bat
+++ push_heroku.bat
@@ -1,2 +1,2 @@
 @ECHO off
-git push https://git.heroku.com/botimus-prime-2am.git master
+git push -f https://git.heroku.com/botimus-prime-2am.git master:master
---
+++ push_heroku_dev.bat
@@ -0,0 +1,2 @@
+@ECHO off
+git push https://git.heroku.com/botimus-prime-2am.git dev:master

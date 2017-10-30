fe735ef6effcb16506f1f8cc84185c18dc72eb40
UnknownAeon <kpfeifle@gmu.edu>
2017-10-30 01:43:18 -0400

    test

--- src/utils.js
+++ src/utils.js
@@ -7,15 +7,15 @@ const commands = require('./router.js');
 
 function getInfo(results) {
   console.log(results.arg);
-  if (results.arg === '!cointoss') return commands.cointoss.info;
-  else if (results.arg === '!exposed') return commands.exposed.info;
-  else if (results.arg === '!diceroll') return commands.diceroll.info;
-  else if (results.arg === '!audio') return commands.audio.info;
-  else if (results.arg === '!help') return commands.help.info;
-  else if (results.arg === '!hots') return commands.hots.info;
-  else if (results.arg === '!insult') return commands.insult.info;
-  else if (results.arg === '!memespeak') return commands.meme.info;
-  else if (results.arg === '!ping') {
+  if (results.arg === 'cointoss') return commands.cointoss.info;
+  else if (results.arg === 'exposed') return commands.exposed.info;
+  else if (results.arg === 'diceroll') return commands.diceroll.info;
+  else if (results.arg === 'audio') return commands.audio.info;
+  else if (results.arg === 'help') return commands.help.info;
+  else if (results.arg === 'hots') return commands.hots.info;
+  else if (results.arg === 'insult') return commands.insult.info;
+  else if (results.arg === 'memespeak') return commands.meme.info;
+  else if (results.arg === 'ping') {
     return {
       argNum : 0,
       name : 'ping',
@@ -23,11 +23,11 @@ function getInfo(results) {
       desc : 'pong!'
     };
   }
-  else if (results.arg === '!rlstats') return commands.rlstats.info;
+  else if (results.arg === 'rlstats') return commands.rlstats.info;
 }
 
 function isValid(command) {
-  var validCommands = ['!cointoss', '!exposed', '!diceroll', '!audio', '!help', '!hots', '!insult', '!memespeak', '!ping', '!rlstats'];
+  var validCommands = ['cointoss', 'exposed', 'diceroll', 'audio', 'help', 'hots', 'insult', 'memespeak', 'ping', 'rlstats'];
   if (validCommands.indexOf(command) > -1) return true;
   else return false;
 }
